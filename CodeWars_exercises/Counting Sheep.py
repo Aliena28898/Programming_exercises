a96602fa2e16dbea4d3b0c9f4cfcf5495b3ea2aa
Aliena28898 <32523698+Aliena28898@users.noreply.github.com>
2017-12-04 13:21:44 +0100

    Create Counting Sheep.py

---
+++ CodeWars_exercises/Counting Sheep.py
@@ -0,0 +1,32 @@
+'''
+Consider an array of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[True,  True,  True,  False,
+  True,  True,  True,  True ,
+  True,  False, True,  False,
+  True,  False, False, True ,
+  True,  True,  True,  True ,
+  False, False, True,  True]
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+'''
+
+
+#SOLUTION:
+
+def count_sheeps(arrayOfSheeps):
+  return arrayOfSheeps.count(True)
+  
+  
+#TESTS:
+array1 = [True,  True,  True,  False,
+          True,  True,  True,  True ,
+          True,  False, True,  False,
+          True,  False, False, True ,
+          True,  True,  True,  True ,
+          False, False, True,  True ];
+              
+test.assert_equals(count_sheeps(array1), 17, "There are 17 sheeps in total, not %s" % count_sheeps(array1))
