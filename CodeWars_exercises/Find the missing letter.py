6aeec44741532d595158546e8d4585f79b6318aa
Aliena28898 <32523698+Aliena28898@users.noreply.github.com>
2017-12-04 13:53:18 +0100

    Create Find the missing letter.py

---
+++ CodeWars_exercises/Find the missing letter.py
@@ -0,0 +1,29 @@
+'''
+Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+
+['a','b','c','d','f'] -> 'e'
+['O','Q','R','S'] -> 'P'
+(Use the English alphabet with 26 letters!)
+'''
+
+
+#SOLUTION:
+
+def find_missing_letter(chars):
+    last = ord(chars[0])-1
+    for letter in chars:
+        if last != ord(letter)-1:
+            return chr(ord(letter)-1)
+        else:
+            last = ord(letter)
+            
+#TESTS:
+test.describe("kata tests")
+test.it("example tests")
+test.assert_equals(find_missing_letter(['a','b','c','d','f']), 'e')
+test.assert_equals(find_missing_letter(['O','Q','R','S']), 'P')
